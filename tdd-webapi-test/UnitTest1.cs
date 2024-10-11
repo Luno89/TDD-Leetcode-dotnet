1d26a48a9408f8b5fd9df1e61859b63087917988
corigan simpson <lunosnyper@gmail.com>
2024-10-11 01:07:33 -0700

    inital working on two-sum

---
+++ tdd-webapi-test/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace tdd_webapi_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+}
